334ed6f97c4e89f44f80d24f3e5e16554018c023
Ji Hee Han <g2hhh2ee@gmail.com>
2023-04-28 13:32:33 +0900

    feat: add ResultContainer and fetch image api

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import Hero from './components/Hero';
 import Footer from './components/Footer';
 
 import './App.css';
+import ResultContainer from './components/ResultContainer';
 
 const Container = styled.div`
-  min-height: 100vh;  
-  position: relative;
-  background-color: var(--primary);
+    min-height: 100vh;
+    position: relative;
+    background-color: var(--primary);
 `;
 
 function App() {
-  return (
-    <Container>
-      <Hero />
-      <Footer />
-    </Container>
-  );
+    return (
+        <Container>
+            <Hero />
+            <ResultContainer />
+            <Footer />
+        </Container>
+    );
 }
 
 export default App;
